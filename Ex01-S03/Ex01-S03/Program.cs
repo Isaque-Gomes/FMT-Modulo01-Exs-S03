0f6fcd2f5b57638dc6402e2708b43baa60fa74ce
Isaque-Gomes <116127894+Isaque-Gomes@users.noreply.github.com>
2023-02-13 23:59:30 -0300

    Exercício 3

--- Ex01-S03/Ex01-S03/Program.cs
+++ Ex01-S03/Ex01-S03/Program.cs
@@ -1,2 +1,7 @@
 ﻿// Criação da classe Ticket
-Console.WriteLine("Hello, World!");
+using Ex01_S03;
+
+Ticket ticket = new Ticket(new DateTime(2022, 10, 02, 20, 05, 20), DateTime.Now, false);
+ticket.CalcularTempo();
+ticket.CalcularValor();
+Console.WriteLine($"O valor é de " + ticket.CalcularValor());
--- Ex01-S03/Ex01-S03/Ticket.cs
+++ Ex01-S03/Ex01-S03/Ticket.cs
@@ -6,7 +6,31 @@ using System.Threading.Tasks;
 
 namespace Ex01_S03
 {
-    internal class Ticket
+    public class Ticket
     {
+        
+        public DateTime Entrada { get; set; }
+        public DateTime Saida { get; set;}
+        public Boolean Ativo { get; set; }
+
+        public double valor = 0.09;
+        public TimeSpan tempo;
+
+        public Ticket(DateTime entrada, DateTime saida, Boolean ativo)
+        {
+            Entrada = entrada;
+            Saida = saida;
+            Ativo = ativo;
+        }
+        public void CalcularTempo()
+        {
+            tempo = Saida - Entrada;
+            Console.WriteLine($"O tempo foi de {tempo.TotalMinutes}");
+        }
+        public double CalcularValor()
+        {
+            double calcValor;
+            return calcValor = valor * tempo.TotalMinutes;
+        }
     }
 }
